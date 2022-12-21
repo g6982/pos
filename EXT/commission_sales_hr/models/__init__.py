3ed4e723cbf1e405c1b97740ad4a9a834032fece
marilyn <millan2031@gmail.com>
2022-12-21 11:29:56 -0400

    deleted

--- EXT/commission_sales_hr/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import hr_payslip,hr_sales_goal, sale, account_move, hr_payslip_comision
--- EXT/custom_pintugama/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import crm_sale_team
-from . import sale
-from . import crm_lead
-from . import mrp_production
-from . import stock_production_lot
